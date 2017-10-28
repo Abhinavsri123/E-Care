39da2e2aa09b6f2a039eb6ad2e028f7212c94678
Shubham Agarwal <Shubham.agarwal7930@gmail.com>
2017-10-29 02:35:10 +0530

    Integreted Map

--- app/src/main/java/com/ecare/newu/e_care/public_portal/trav_details.java
+++ app/src/main/java/com/ecare/newu/e_care/public_portal/trav_details.java
@@ -6,14 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.ecare.newu.e_care.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class trav_details extends Fragment {
 
+    Spinner s1;
+    String type[] = {"Select Vehical Type","Taxi","Auto","Ricksaw","Private Car","Rental Car"};
+    TextView t1,t2;
 
     public trav_details() {
         // Required empty public constructor
@@ -25,7 +34,28 @@ public class trav_details extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_trav_details, container, false);
+
+        s1 = (Spinner) v.findViewById(R.id.spinner);
+        t1= (TextView) v.findViewById(R.id.textView27);
+        t2= (TextView) v.findViewById(R.id.textView28);
+        t1.setText(getdate());
+        t2.setText(gettime());
+        ArrayAdapter a1 = new ArrayAdapter(getContext(),R.layout.support_simple_spinner_dropdown_item,type);
+        s1.setAdapter(a1);
         return v;
     }
 
+    public String getdate()
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDateandTime = sdf.format(new Date());
+        return currentDateandTime;
+    }
+
+    public String gettime()
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String currentDateandTime = sdf.format(new Date());
+        return currentDateandTime;
+    }
 }
---
+++ app/src/main/java/url.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Shubham on 29-Oct-17.
+ */
+
+public class url {
+
+    String url_link = "";
+
+    public String geturl()
+    {
+        return url_link;
+    }
+}
