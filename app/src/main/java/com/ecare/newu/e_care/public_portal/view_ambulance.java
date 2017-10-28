bd78baf3ec0da48574369f288aea0bbe099be302
Shubham Agarwal <Shubham.agarwal7930@gmail.com>
2017-10-29 00:05:02 +0530

    Integreted Map

--- app/src/main/java/com/ecare/newu/e_care/public_portal/view_ambulance.java
+++ app/src/main/java/com/ecare/newu/e_care/public_portal/view_ambulance.java
@@ -1,20 +1,39 @@
 package com.ecare.newu.e_care.public_portal;
 
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.ecare.newu.e_care.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class view_ambulance extends Fragment {
+public class view_ambulance extends Fragment implements OnMapReadyCallback{
 
-
+    GoogleMap mgoogleMap;
+    MapView mapView;
+    View v;
+    String stringLatitude ="";
+    String stringLongitude ="";
     public view_ambulance() {
         // Required empty public constructor
     }
@@ -24,8 +43,44 @@ public class view_ambulance extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_view_ambulance, container, false);
+        v= inflater.inflate(R.layout.fragment_view_ambulance, container, false);
+
+        Button b1= (Button) v.findViewById(R.id.button5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Contacting NearBy Ambulance...", Toast.LENGTH_SHORT).show();
+            }
+        });
         return v;
     }
 
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) v.findViewById(R.id.map);
+        if(mapView != null)
+        {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+
+
+        MapsInitializer.initialize(getContext());
+        mgoogleMap = googleMap;
+
+        mgoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(20.359202,85.822389)).title("Near By Ambulance").snippet("Ambulance"));
+        CameraPosition amb = CameraPosition.builder().target(new LatLng(20.359202,85.822389)).zoom(16).bearing(0).tilt(0).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(amb));
+    }
 }
