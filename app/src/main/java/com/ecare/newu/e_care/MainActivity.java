2df9f4f6e01f74e01aa7d68c32ec0bb568883d92
Shubham Agarwal <Shubham.agarwal7930@gmail.com>
2017-10-28 16:55:14 +0530

    Next

--- app/src/main/java/com/ecare/newu/e_care/MainActivity.java
+++ app/src/main/java/com/ecare/newu/e_care/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ecare.newu.e_care;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ecare.newu.e_care.public_portal.public_main;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, public_main.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/ecare/newu/e_care/public_portal/public_main.java
+++ app/src/main/java/com/ecare/newu/e_care/public_portal/public_main.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ecare.newu.e_care.R;
+
 public class public_main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
