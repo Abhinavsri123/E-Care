a643540a6f8225741327040360d9175b516ab3d0
E Care <abhinavsrivastava54321@gmail.com>
2017-10-28 20:26:00 +0530

    again2

--- app/src/main/java/com/ecare/newu/e_care/public_portal/submit_image.java
+++ app/src/main/java/com/ecare/newu/e_care/public_portal/submit_image.java
@@ -1,19 +1,39 @@
 package com.ecare.newu.e_care.public_portal;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.ecare.newu.e_care.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class submit_image extends Fragment {
-
+    ImageButton imageButton;
+    ImageView imageView;
+    Button button;
+    int request_code = 1;
+    Bitmap bitmap;
 
     public submit_image() {
         // Required empty public constructor
@@ -24,10 +44,66 @@ public class submit_image extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_submit_image, container, false);
+        View v= inflater.inflate(R.layout.fragment_submit_image, container, false);
 
+        imageButton= (ImageButton) v.findViewById(R.id.tv);
+        button = (Button) v.findViewById(R.id.button1);
+        reqAskPermission(Manifest.permission.RECEIVE_SMS, 1001);
+        reqAskPermission(Manifest.permission.SEND_SMS, 1002);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i, request_code);
+            }
+
+        });
 
         return v;
     }
 
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == request_code) ;
+        {
+            Bundle b = data.getExtras();
+            Bitmap bmp = (Bitmap) b.get("data");
+            imageButton.setImageBitmap(bmp);
+        }
+
+    }
+
+    private void reqAskPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, requestCode);
+        } else
+
+        {
+            Toast.makeText(getContext(), "Permission_Granted", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1001:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+
+                }
+                break;
+            case 1002:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+
+                }
+                break;
+            default:
+
+        }
+        return;
+    }
 }
