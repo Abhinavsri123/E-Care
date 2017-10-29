58c407b2fbcc7d77598c2c0d00ca60ebda808216
E Care <abhinavsrivastava54321@gmail.com>
2017-10-29 08:21:34 +0530

    again8

--- app/src/main/java/com/ecare/newu/e_care/police/ambulances_near_you.java
+++ app/src/main/java/com/ecare/newu/e_care/police/ambulances_near_you.java
@@ -2,19 +2,32 @@ package com.ecare.newu.e_care.police;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ecare.newu.e_care.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ambulances_near_you extends Fragment {
+public class ambulances_near_you extends Fragment implements OnMapReadyCallback {
 
-
+    GoogleMap mgoogleMap;
+    MapView mapView;
+    View v;
+    String stringLatitude ="";
+    String stringLongitude ="";
     public ambulances_near_you() {
         // Required empty public constructor
     }
@@ -26,6 +39,28 @@ public class ambulances_near_you extends Fragment {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_ambulances_near_you, container, false);
         return v;
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) v.findViewById(R.id.map3);
+        if(mapView != null)
+        {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mgoogleMap = googleMap;
+
+        mgoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(20.359202,85.822389)).title("Ambulances Near You").snippet("Ambulance"));
+        CameraPosition amb = CameraPosition.builder().target(new LatLng(20.359202,85.822389)).zoom(16).bearing(0).tilt(0).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(amb));
     }
 
 }
